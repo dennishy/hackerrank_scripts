c7e881afbdb6b09ca4c1f7e0633564c9f623c8ec
Dennis <dennis.y.hong@gmail.com>
2020-01-06 23:48:24 -0500

    stats exercises d1

---
+++ d1.py
@@ -0,0 +1,30 @@
+#test1 = 9
+#test2 = '3 7 8 5 12 14 21 13 18'
+
+
+
+def median(x):
+    l = len(x)
+
+    if l % 2 == 0:
+        m1 = l // 2 
+        m2 = (l // 2) - 1 
+
+        m = (x[m1] + x[m2])//2
+
+    else:
+        m = int(round(l/2))
+        m = x[m]
+
+    return(m)
+
+if __name__ == '__main__':
+
+    k = int(input())
+    x = sorted(list(map(int, input().split() )))
+
+
+    print(median(x[:k//2]))
+    print(median(x))
+    print(median(x[(k//2)+1:]))
+    
